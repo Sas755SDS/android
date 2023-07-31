8d1f1b5804e64e24c82e6cd1e0af7dfb8f356722
sahil <sahilmiya731@gmail.com>
2023-07-31 19:31:01 +0530

    second commit

---
+++ app/src/main/java/com/example/saumiyaapp/implicit.kt
@@ -0,0 +1,11 @@
+package com.example.saumiyaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class implicit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_implicit)
+    }
+}
